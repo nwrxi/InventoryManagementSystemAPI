f3b30f4a0f3043adc901beaae12c58c88466f91f
Aleks <aleksf11125@outlook.com>
2021-02-06 11:21:37 +0100

    Add JWT Token Generator

---
+++ InventoryManagementSystemAPI.Security/TokenGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using InventoryManagementSystemAPI.Data.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace InventoryManagementSystemAPI.Security
+{
+    public class TokenGenerator
+    {
+        private readonly SymmetricSecurityKey _key;
+
+        public TokenGenerator(IConfiguration configuration)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["TokenSecretKey"]));
+        }
+
+        public string GenerateToken(User user)
+        {
+            var descriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new List<Claim> {
+                    new(JwtRegisteredClaimNames.NameId, user.Id)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature)
+            };
+
+            var handler = new JwtSecurityTokenHandler();
+            var token = handler.CreateJwtSecurityToken(descriptor);
+
+            return handler.WriteToken(token);
+        }
+    }
+}
